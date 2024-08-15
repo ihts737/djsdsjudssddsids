ddc0323063da6feb2abc726a53e77c70b2c5c971
ihts737 <gwest6365@gmail.com>
2024-08-15 06:42:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def home():
     return "I'm alive"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.getenv("PORT", 8080))  
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
